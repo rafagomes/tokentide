e534ef2fffc24ee78ae00d3f0f5979a35f80b126
rafagomes <rafaelgomes.web@gmail.com>
2024-09-27 15:08:19 -0300

    Add initial project files and contracts

---
+++ solidity/contracts/__mocks__/MockERC1155.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract MockERC1155 is ERC1155 {
+    constructor() ERC1155("https://token-cdn-domain/{id}.json") {
+        _mint(msg.sender, 1, 100, "");
+    }
+}
---
+++ solidity/hardhat.config.ts
@@ -0,0 +1,9 @@
+import "@nomiclabs/hardhat-ethers";
+import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.27",
+};
+
+export default config;
