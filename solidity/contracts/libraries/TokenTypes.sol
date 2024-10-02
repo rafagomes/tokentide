5cb1239590cc6048b37cf5e794d54754288e0c71
rafagomes <rafaelgomes.web@gmail.com>
2024-10-01 22:32:02 -0300

    Add TokenTypes library for token type enumeration

---
+++ solidity/contracts/libraries/TokenTypes.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+library TokenTypes {
+    enum TokenType {
+        UNKNOWN,
+        ERC20,
+        ERC721,
+        ERC1155
+    }
+}
