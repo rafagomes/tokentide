aba2328aaeb1434a9591fa2910b8d99a9c6c5dff
rafagomes <rafaelgomes.web@gmail.com>
2024-10-01 22:31:55 -0300

    Refactor ITokenIdentifier interface to include the identifyTokenType function

---
+++ solidity/contracts/interfaces/ITokenIdentifier.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+import '../libraries/TokenTypes.sol';
+
+/**
+ * @title ITokenIdentifier
+ * @dev Interface for the TokenIdentifier contract
+ */
+interface ITokenIdentifier {
+    function identifyTokenType(
+        address token
+    ) external returns (TokenTypes.TokenType);
+}
