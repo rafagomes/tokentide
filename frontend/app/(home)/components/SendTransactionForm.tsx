164a6bf5a5e3e456dfc0f4c3e8bb8d14826e55db
rafagomes <rafaelgomes.web@gmail.com>
2024-10-04 22:12:15 -0300

    Refactor SendTransactionForm component for improved code consistency and readability

--- frontend/app/(home)/components/SendTransactionForm.tsx
+++ frontend/app/(home)/components/SendTransactionForm.tsx
@@ -1,31 +1,31 @@
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm, useWatch } from 'react-hook-form';
-import { z } from 'zod';
-import { Button } from '@/components/ui/button';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, useWatch } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
   FormLabel,
-  FormMessage
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { isAddress, parseEther } from 'viem';
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { isAddress, parseEther } from "viem";
 import {
   useAccount,
   useBalance,
   useSendTransaction,
-  useWaitForTransactionReceipt
-} from 'wagmi';
-import { useEffect } from 'react';
-import { Label } from '@/components/ui/label';
+  useWaitForTransactionReceipt,
+} from "wagmi";
+import { useEffect } from "react";
+import { Label } from "@/components/ui/label";
 
 const formSchema = z.object({
   to: z.string().refine((value) => isAddress(value), {
-    message: 'Invalid Address'
+    message: "Invalid Address",
   }),
-  value: z.string()
+  value: z.string(),
 });
 
 export default function SendTransactionForm() {
@@ -34,29 +34,29 @@ export default function SendTransactionForm() {
   const { sendTransaction, data: hash } = useSendTransaction();
   const { isLoading: isConfirming, isSuccess: isConfirmed } =
     useWaitForTransactionReceipt({
-      hash
+      hash,
     });
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      to: '',
-      value: ''
-    }
+      to: "",
+      value: "",
+    },
   });
 
   const value = useWatch({
     control: form.control,
-    name: 'value'
+    name: "value",
   });
 
   useEffect(() => {
     const isInsufficientBalance = balance && balance?.value < parseEther(value);
 
     if (isInsufficientBalance) {
-      form.setError('value', { message: 'Insufficient balance' });
+      form.setError("value", { message: "Insufficient balance" });
     } else {
-      form.clearErrors('value');
+      form.clearErrors("value");
     }
   }, [value, balance, form]);
 
@@ -68,17 +68,17 @@ export default function SendTransactionForm() {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className='space-y-10 w-full relative'
+        className="space-y-10 w-full relative"
       >
         <FormField
           control={form.control}
-          name='to'
+          name="to"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Address</FormLabel>
               <FormControl>
                 <Input
-                  placeholder='0x0000000000000000000000000000'
+                  placeholder="0x0000000000000000000000000000"
                   {...field}
                 />
               </FormControl>
@@ -88,26 +88,26 @@ export default function SendTransactionForm() {
         />
         <FormField
           control={form.control}
-          name='value'
+          name="value"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Value</FormLabel>
               <FormControl>
-                <Input placeholder='0.1' {...field} type='number' />
+                <Input placeholder="0.1" {...field} type="number" />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
         {(isConfirming || isConfirmed) && (
-          <Label className='absolute bottom-12 text-sky-500'>
-            {isConfirming ? 'Confirming Tx...' : 'Confirmed'}
+          <Label className="absolute bottom-12 text-sky-500">
+            {isConfirming ? "Confirming Tx..." : "Confirmed"}
           </Label>
         )}
         <Button
           disabled={!form.formState.isValid || isConfirming || isConfirmed}
-          className='w-full'
-          type='submit'
+          className="w-full"
+          type="submit"
         >
           Send
         </Button>
